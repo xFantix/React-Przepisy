736a765905066366b3161642a2ead6f40a3a3323
Kamil Jankowski <kamil.jankoowski@outlook.com>
2020-12-20 20:36:02 +0100

    make recipeCard for mobile and desktop

--- src/components/RecipeInformation.js
+++ src/components/RecipeInformation.js
@@ -1,62 +1,76 @@
-import React,{useEffect, useState} from 'react';
-import * as style from '../style/recipeInformationStyle';
+import React, { useEffect, useState } from "react";
+import * as style from "../style/recipeInformationStyle";
 const RecipeInformation = (props) => {
-  const{id} = props.match.params;
-  
+  const { id } = props.match.params;
+
   const [recipeInformation, setRecipeInformation] = useState({
-    loading:true,
-    information:{},
-  })
+    loading: true,
+    information: {},
+  });
 
-    useEffect(()=>{
+  useEffect(() => {
+    const API = `https://api.spoonacular.com/recipes/${id}/information?apiKey=210c10f0f72a4f999d2c566a257ce155`;
 
-      const API = `https://api.spoonacular.com/recipes/${id}/information?apiKey=210c10f0f72a4f999d2c566a257ce155`
-      
-      fetch(API)
-        .then((response) => {
-          if (response.ok) {
-            return response;
-          }
-          throw Error("Dont work :(");
-        })
-        .then((response) => response.json())
-        .then((data) => {
-         console.log(data);
-          setRecipeInformation({
-            information:{...data},
-            loading:false,
-          })
-         
+    fetch(API)
+      .then((response) => {
+        if (response.ok) {
+          return response;
+        }
+        throw Error("Dont work :(");
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setRecipeInformation({
+          information: { ...data },
+          loading: false,
         });
-        
-      },[]);
+      });
+  }, []);
 
-      
-
-      return (
-        <>
-        {!recipeInformation.loading &&
+  return (
+    <>
+      {!recipeInformation.loading && (
         <style.Container>
-        <style.ContainerInformation>
-          <style.MainHeader>{recipeInformation.information.title}</style.MainHeader>
-          <style.SmallHeader>{recipeInformation.information.dishTypes}</style.SmallHeader>
-            <style.IngredientsHeader>Ingredients</style.IngredientsHeader>
-            <style.ListIngredients>
-            { 
-              recipeInformation.information.extendedIngredients.map(el=>
-                <li>{el.originalName}</li>
-                )
-            }
-            </style.ListIngredients>
-        </style.ContainerInformation>
-        <style.MealImage bgImage={recipeInformation.information.image}>
-        </style.MealImage>
-      </style.Container>}
-      </>
-          
-      );
-    
-       
-}
- 
+          <style.Main>
+            <style.ContainerInformation>
+              <style.MainHeader>
+                {recipeInformation.information.title}
+              </style.MainHeader>
+              <style.SmallHeader>
+                {recipeInformation.information.dishTypes.map((el) => (
+                  <p>{el}</p>
+                ))}
+              </style.SmallHeader>
+              <style.IngredientsHeader>Ingredients</style.IngredientsHeader>
+              <style.ListIngredients>
+                {recipeInformation.information.extendedIngredients.map((el) => (
+                  <li>{el.originalName}</li>
+                ))}
+              </style.ListIngredients>
+            </style.ContainerInformation>
+            <style.MealImage
+              bgImage={recipeInformation.information.image}
+            >
+              <style.MainHeaderMobile>{recipeInformation.information.title}</style.MainHeaderMobile>
+              <style.ListIngredientsMobile>
+              {recipeInformation.information.extendedIngredients.map((el) => (
+                  <li>{el.originalName}</li>
+                ))}
+              </style.ListIngredientsMobile>
+              <style.InformationBar>
+                  <style.InformationBarParagraph>{recipeInformation.information.readyInMinutes} minutes</style.InformationBarParagraph>
+                  <style.InformationBarParagraph>{recipeInformation.information.servings} servings</style.InformationBarParagraph>
+              </style.InformationBar>
+            </style.MealImage>
+          </style.Main>
+          <style.RecipeListContainer>
+            {recipeInformation.information.analyzedInstructions.map(el=>(el.steps.map(el=>(<li>{el.step}</li>))))}
+          </style.RecipeListContainer>
+        </style.Container>
+      )}
+    </>
+  );
+};
+
 export default RecipeInformation;
--- src/style/recipeInformationStyle.js
+++ src/style/recipeInformationStyle.js
@@ -1,25 +1,35 @@
 import styled from 'styled-components';
 const fontMontserrat = "'Montserrat', sans-serif";
-export const Container = styled.div`
+export const Main = styled.div`
     width:100%;
     display:flex;
     min-height:calc(100vh - 56px);
 `
+export const Container = styled.div`
+    width:100%;
+    display:flex;
+    flex-direction:column;
+    
+`
 export const ContainerInformation = styled.div`
     width:40%;
     background-color:#F8F8F8;
+    @media(max-width:860px)
+    {
+        display:none;
+    }
 `
 export const MainHeader = styled.h2`
     font-family:${fontMontserrat};
-    padding:50px 10px 0px 10px;
+    padding:50px 15px 0px 15px;
     color:#FFD71D;
     font-weight:700;
-    font-size:30px;
+    font-size:35px;
     text-transform:uppercase;
 `
 export const SmallHeader = styled.h3`
     font-family:${fontMontserrat};
-    padding-left:10px;
+    padding-left:15px;
     padding-top:10px;
     color:#393A3B;
     font-weight:400;
@@ -28,7 +38,7 @@ export const SmallHeader = styled.h3`
 `
 export const IngredientsHeader = styled.h3`
     font-family:${fontMontserrat};
-    padding-left:10px;
+    padding-left:15px;
     padding-top:40px;
     color:#393A3B;
     font-size:25px;
@@ -39,9 +49,67 @@ export const  MealImage = styled.div`
     background-image:url(${props=>props.bgImage});
     background-position:center;
     background-size:cover;
+    position: relative;
+    @media(max-width:860px)
+    {
+        width:100%;
+        background:linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.405)),url(${props=>props.bgImage});
+        background-position:center;
+        background-size:cover;
+        background-repeat:no-repeat;
+        display:flex;
+        flex-direction:column;
+    }
 `
 export const ListIngredients = styled.ul`
     padding:20px 0px 0px 35px;
     font-family:${fontMontserrat};
-    font-size:25px;
+    font-size:20px;
 `
+export const InformationBar = styled.div`
+    width:100%;
+    justify-content:space-around;
+    align-items:center;
+    background-color:#FFD71D;
+    position:absolute;
+    bottom:0;
+    display:flex;
+`
+export const InformationBarParagraph = styled.div`
+    font-family:${fontMontserrat};
+    font-size:20px;
+    padding:10px 0px;
+`
+export const RecipeListContainer = styled.ol`
+    padding:30px 30px;
+    font-family:${fontMontserrat};
+    &>li{
+        padding-top:10px;
+    }
+`
+export const MainHeaderMobile = styled.h2`
+ font-family:${fontMontserrat};
+    padding:50px 0px;
+    color:#FFD71D;
+    font-weight:700;
+    font-size:45px;
+    text-transform:uppercase;
+    text-align:center;
+    @media(min-width:861px){
+        display:none;
+    }
+`
+export const ListIngredientsMobile = styled.ul`
+    text-align:center;
+    font-family:${fontMontserrat};
+    font-size:25px;
+    text-transform:uppercase;
+    letter-spacing:3px;
+    list-style:none;
+    padding-bottom:95px;
+    color:white;
+    @media(min-width:861px){
+        display:none;
+    }
+`
+
